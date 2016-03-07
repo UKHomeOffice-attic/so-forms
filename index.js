2ef33e598f1340c6efbe292ad915ad73dca7051c
Joe Flintham <joeflintham@gmail.com>
2016-03-07 15:10:22 +0000

    Export controllers

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var controllers = {
+
+  ajax: require('./lib/controllers/ajax-controller.js'),
+  ajax_edit: require('./lib/controllers/ajax-edit-controller.js'),
+  base: require('./lib/controllers/base-controller.js'),
+  error: require('./lib/controllers/base-error.js'),
+  date: require('./lib/controllers/date-controller.js'),
+  edit: require('./lib/controllers/edit-controller.js'),
+  logger: require('./lib/controllers/logger.js'),
+  start: require('./lib/controllers/start-controller.js')
+  
+};
+
+module.exports = {
+  controllers: controllers
+};
